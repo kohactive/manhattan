6e1766e2b5fdb6a97a644497e15f5f0e05c65dd5
Andy Richardson <andy@kohactive.com>
2014-05-13 14:32:46 -0500

    added ie stylesheet to precompile list

--- lib/manhattan/engine.rb
+++ lib/manhattan/engine.rb
@@ -1,4 +1,7 @@
 module Manhattan
   class Engine < ::Rails::Engine
+    initializer 'Manhattan precompile hook', :group => :all do |app|
+      app.config.assets.precompile += ['ie.css']
+    end
   end
 end
